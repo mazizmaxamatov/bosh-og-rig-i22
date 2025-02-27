59039b6de5cb9c875e759fa074af21514d6513ec
muxammadazizjon <mazizmaxamatov@gmail.com>
2025-02-27 08:43:20 +0500

    zaybal

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,7 @@ export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      backgroundImage: {
-        logo: "url('/src/Assets/img/hidr.png')",
-        img: "url('/src/Assets/img/burger.png')"
-      }
+     
     },
   },
   plugins: [],
